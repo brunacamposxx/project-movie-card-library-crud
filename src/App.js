2501550b7a8c9595e4aed204b15c9cf175564ee8
Bruna Campos <92brunacampos@gmail.com>
2021-05-25 15:26:45 -0300

    com o switch nao passou no teste [App.js]

--- src/App.js
+++ src/App.js
@@ -10,31 +10,29 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <div>Movie Card Library CRUD</div>
-        <Route
-          exact
-          path="/"
-          component={ MovieList }
-        />
-        <Route
-          exact
-          path="/movies/:id"
-          component={ MovieDetails }
-        />
-        <Route
-          path="/movies/new"
-          component={ NewMovie }
-        />
-        <Route
-          exact
-          path="/movies/:id/edit"
-          component={ EditMovie }
-        />
-        <Route
-          component={ NotFound }
-        />
-      </Switch>
+      <div>Movie Card Library CRUD</div>
+      <Route
+        exact
+        path="/"
+        component={ MovieList }
+      />
+      <Route
+        exact
+        path="/movies/:id"
+        component={ MovieDetails }
+      />
+      <Route
+        path="/movies/new"
+        component={ NewMovie }
+      />
+      <Route
+        exact
+        path="/movies/:id/edit"
+        component={ EditMovie }
+      />
+      <Route
+        component={ NotFound }
+      />
     </BrowserRouter>
   );
 }
