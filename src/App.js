0adee9c0ab4eef960b0118e5324978b5d1dd8fb1
Bruna Campos <92brunacampos@gmail.com>
2021-05-27 15:28:37 -0300

    Refazendo primeiro requisito com o Wolf no plantao especial =)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
@@ -11,28 +11,30 @@ function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-      <Route
-        exact
-        path="/"
-        component={ MovieList }
-      />
-      <Route
-        exact
-        path="/movies/:id"
-        component={ MovieDetails }
-      />
-      <Route
-        path="/movies/new"
-        component={ NewMovie }
-      />
-      <Route
-        exact
-        path="/movies/:id/edit"
-        component={ EditMovie }
-      />
-      <Route
-        component={ NotFound }
-      />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={ MovieList }
+        />
+        <Route
+          path="/movies/new"
+          component={ NewMovie }
+        />
+        <Route
+          exact
+          path="/movies/:id"
+          component={ MovieDetails }
+        />
+        <Route
+          exact
+          path="/movies/:id/edit"
+          component={ EditMovie }
+        />
+        <Route
+          component={ NotFound }
+        />
+      </Switch>
     </BrowserRouter>
   );
 }
